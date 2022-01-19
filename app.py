829d4f2f835c6cf74c8bef52f2b4e87502749eb6
nisarg <nisarg1463@gmail.com>
2022-01-19 14:31:32 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
+import wolframalpha
+import wikipedia
 
 app = Flask(__name__)
-@app.route('/test',methods = ['GET'])
+
+
+@app.route('/test', methods=['GET'])
 def return_request():
-    d={}
+    d = {}
     input_chr = int(request.args['query'])
     answer = f'{input_chr**2}'
     d['output'] = answer
     return jsonify(d)
 
+
+@app.route('/search', methods=['GET'])
+def search():
+    output = {}
+    client = wolframalpha.Client('3HLRTJ-A7P8K5RQW8')
+    query = str(request.args['query'])
+    res = client.query(query)
+    wolframalpha_answers = []
+    for i in res.results:
+        wolframalpha_answers.append(i.text)
+    output['wolframalpha answers'] = wolframalpha_answers
+
+    print(query)
+    page = wikipedia.page(query)
+    wiki_search_results = wikipedia.search(query)
+    wikipedia_answers = {}
+    wikipedia_answers['search'] = wiki_search_results
+    wikipedia_answers['page'] = {'title': page.title,
+                                 'content': page.content, 'summary': page.summary}
+    output['wikipedia answers'] = wikipedia_answers
+
+    return jsonify(output)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
